a3ddcfc8bdebec639c2085b99664edcc3c4821e9
Kunguiner <274454442@qq.com>
2020-01-19 16:26:44 +0800

    配置跨域请求前缀

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,18 @@ module.exports = {
     devServer: {
         host: 'localhost',
         port: 9999,
-    }
+        proxy: {
+            '/vhr': {
+                // 转发到的地址
+                target: 'http://localhost',
+                ws: true,
+                changeOrigin: true,
+                // 以什么开头的路径会被转发到 target
+                pathRewrite: {
+                    '^/vhr': ''
+                }
+            }
+        }
+    },
+
 }
