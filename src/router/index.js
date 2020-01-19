916e59a78d88bec21076c2b823e7beceb26113cd
Kunguiner <274454442@qq.com>
2020-01-19 23:06:40 +0800

    配置前置守卫, 指定哪些路由需要做登录拦截

--- src/router/index.js
+++ src/router/index.js
@@ -8,20 +8,34 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    meta : {
+      needLogin: true,
+    },
   },
   {
     path: '/about',
     name: 'about',
+    meta : {
+      needLogin: true,
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
+  {
+    path : '/login',
+    name : 'login',
+    component : () => import('../views/Login')
+  },
   // TODO 测试路由
   {
     path : '/vfor',
     name : 'vfor-test',
+    meta : {
+      needLogin: false,
+    },
     component : () => import('../components/VFor.vue')
   },
   {
@@ -34,5 +48,15 @@ const routes = [
 const router = new VueRouter({
   routes
 })
-
+router.beforeEach((to, from, next) => {
+  // ...
+  console.log('路由守卫',to);
+  let meta = to.meta;
+  if(meta.needLogin){
+    alert('需要登录才能访问');
+    next('/login')
+  }else {
+    next();
+  }
+})
 export default router
