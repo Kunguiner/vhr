97b5aa3f6c6f06f3b85a34828b898ac7f26b7d69
Kunguiner <274454442@qq.com>
2020-01-19 23:03:58 +0800

    引入element-ui依赖

--- src/main.js
+++ src/main.js
@@ -3,13 +3,26 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import {
+    Form,
+    FormItem,
+    Input,
+    InputNumber,
+    Button
+} from 'element-ui';
+
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(InputNumber);
+Vue.use(Button);
 Vue.prototype.axios = axios;
 Vue.config.productionTip = false
 
 const vue = new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 });
 vue.$mount('#app');
 window.vue = vue;
