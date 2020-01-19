eb1d5e7da3d35d41de7df69b2c83d89ca16dab73
Kunguiner <274454442@qq.com>
2020-01-19 23:01:24 +0800

    测试nginx配置的跨域请求是否有用

--- src/components/VFor.vue
+++ src/components/VFor.vue
@@ -1,31 +1,37 @@
 <template>
-  <div>
+    <div>
 
-    <button>发送请求</button>
-  </div>
+        <button @click="sendRequest">发送请求</button>
+    </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
+    export default {
+        data() {
+            return {
 
-        newBook: {
-          author: '',
-          name: ''
+                newBook: {
+                    author: '',
+                    name: ''
+                }
+            }
+        },
+        methods: {
+            sendRequest() {
+                let url = "/vhr /vue"
+                this.axios.get(url)
+                    .then(function (data) {
+                        alert('请求成功');
+                        console.log(data);
+                    }).catch(function (error) {
+                        alert("请求失败")
+                })
+            }
+        },
+        mounted() {
+            window.vfor = this.$data;
         }
-      }
-    },
-    methods: {
-      sendRequest(){
-        let url = "localhost:8888/vue"
-
-      }
-    },
-    mounted() {
-      window.vfor = this.$data;
     }
-  }
 </script>
 
 <style>
